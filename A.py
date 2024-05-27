128b2368a33d2eb4bdb98a36d030511d51d9e3c6
Murilo2305 <162511738+Murilo2305@users.noreply.github.com>
2024-05-27 08:44:53 -0300

    Add files via upload
    A

---
+++ A.py
@@ -0,0 +1,12 @@
+num1 = float (input("Digite seu primeiro número"))
+num2 =float (input("Digite o segundo número"))
+
+soma = num1 + num2
+diferença = num1 - num2
+produto = num1 * num2
+quociente = num1/num2
+
+print(f"Soma: {soma}")
+print(f"diferença: {diferença}")
+print(f"produto: {produto}")
+print(f"quociente: {quociente}")
