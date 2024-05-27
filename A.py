f8c3800535fd9b4986684139efbc4296208c7fb0
Murilo2305 <162511738+Murilo2305@users.noreply.github.com>
2024-05-27 08:57:07 -0300

    Update A.py
    A

--- A.py
+++ A.py
@@ -1,11 +1,13 @@
 num1 = float (input("Digite seu primeiro número"))
 num2 =float (input("Digite o segundo número"))
+nome = string(input("Diga seu nome")
 
 soma = num1 + num2
 diferença = num1 - num2
 produto = num1 * num2
 quociente = num1/num2
 
+print(f"Feito por {nome}")
 print(f"Soma: {soma}")
 print(f"diferença: {diferença}")
 print(f"produto: {produto}")
